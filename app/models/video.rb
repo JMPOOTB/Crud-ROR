6e2fc0b2090ddfdeddfdd8d6882e6c8956ab4ba9
jpoot <jpoot@outlook.com>
2016-03-31 10:11:42 -0600

    Crud Ruby on Rails
    Crud en Ruby On Rails

---
+++ app/models/video.rb
@@ -0,0 +1,15 @@
+class Video < ActiveRecord::Base
+
+
+def next
+Video.where("id > ?",id).first
+
+end
+
+
+def prev
+
+Video.where("id < ?",id).last
+end
+
+end
---
+++ db/migrate/20151115022336_videos.rb
@@ -0,0 +1,4 @@
+class Videos < ActiveRecord::Migration
+  def change
+  end
+end
